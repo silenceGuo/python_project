5e478c923113d9b8e129644ece6373ccc7432a5f
silenceGuo <516605659@qq.com>
2018-03-12 20:53:41 +0800

    增加配置文件，代码函数重构

--- deploy.py
+++ deploy.py
@@ -7,10 +7,11 @@ import os
 import sys
 import string
 import psutil
-import re
+import platform
 import signal
 
 def getPid(serverName):
+    # python 方式获取 服务名称或者进程名字的pid
     for pid in psutil.pids():
         p = psutil.Process(pid)
         #print p.name()
@@ -18,7 +19,7 @@ def getPid(serverName):
             print "Pid:%s,PidName:%s " % (pid,p.name())
             return pid
     print "Not Find Servername:%s" % serverName
-    return None
+    #return None
 
 def kill(pid):
     try:
@@ -30,10 +31,38 @@ def kill(pid):
         print '没有如此进程!!!'
         sys.exit()
 
+def TestPlatform():
+    print ("----------Operation System--------------------------")
+    #Windows will be : (32bit, WindowsPE)
+    #Linux will be : (32bit, ELF)
+    print(platform.architecture())
+
+    #Windows will be : Windows-XP-5.1.2600-SP3 or Windows-post2008Server-6.1.7600
+    #Linux will be : Linux-2.6.18-128.el5-i686-with-redhat-5.3-Final
+    print(platform.platform())
+
+    #Windows will be : Windows
+    #Linux will be : Linux
+    print(platform.system())
+
+    print ("--------------Python Version-------------------------")
+    #Windows and Linux will be : 3.1.1 or 3.1.3
+    print(platform.python_version())
+
+def UsePlatform():
+  sysstr = platform.system()
+  if(sysstr =="Windows"):
+    print ("Call Windows tasks")
+  elif(sysstr == "Linux"):
+    print ("Call Linux tasks")
+  else:
+    print ("Other System tasks")
+
 if __name__ == "__main__":
 
     pid = getPid(serverName="cmd.exe")
-    kill(pid)
+    #kill(pid)
+    TestPlatform()
 
 
    # print p
