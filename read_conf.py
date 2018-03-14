ae95ed8658cfd08085991d3b5a95dd02ffdd08b5
silenceGuo <516605659@qq.com>
2018-03-14 17:11:59 +0800

    ｕｐｄａｔｅ

--- read_conf.py
+++ read_conf.py
@@ -8,28 +8,35 @@ import os
 import sys
 #部署服务和端口配置文件 server.conf
 serverConf = "server.conf"
-def readConf():
+def readConf(type):
     import ConfigParser
     cf = ConfigParser.ConfigParser()
     cf.read(serverConf)
     serverNameDict = {}
     portDict = {}
+    # print cf.sections()
+    # print cf.options("conf")
+    # for optin in cf.options("conf"):
+    #     print optin
+    #     print cf.get("conf",optin)
     for serverName in cf.sections():
-        print 'serverName:%s' % serverName
+        #print 'serverName:%s' % serverName
         for optins in cf.options(serverName):
             # 取服务名下的对应的配置和参数
             port = cf.get(serverName, optins)
             print optins, port
+            #print portDict[optins]
             portDict[optins] = port
         serverNameDict[serverName] = portDict
     return serverNameDict
+
 if __name__ == "__main__":
-    if not os.path.exists(os.path.join(os.getcwd(),serverConf)):
-        print "serverconf is not exists,check serverconf"
-        print """ %s like this:
-                   [servername]
-                   http_port = 8810
-                   ajp_port = 8820
-                   shutdown_port = 8830""" % serverConf
-        sys.exit()
-    print readConf()
+    # if not os.path.exists(os.path.join(os.getcwd(),serverConf)):
+    #     print "serverconf is not exists,check serverconf"
+    #     print """ %s like this:
+    #                [servername]
+    #                http_port = 8810
+    #                ajp_port = 8820
+    #                shutdown_port = 8830""" % serverConf
+    #     sys.exit()
+    print readConf(type)
