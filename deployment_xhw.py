ab1ed8ae04a3e98abd61491b001b24725478927b
silenceGuo <516605659@qq.com>
2018-03-15 17:37:58 +0800

    ｕｐｄａｔｅ

--- deployment_xhw.py
+++ deployment_xhw.py
@@ -14,6 +14,7 @@ sys.setdefaultencoding('utf-8')
 #from xml.dom.minidom import parse
 import xml.dom.minidom
 import codecs
+import ConfigParser
 from subprocess import PIPE,Popen
 
 # # 部署的目录
@@ -27,6 +28,7 @@ from subprocess import PIPE,Popen
 deploydir = "webapps"
 #部署服务和端口配置文件 server.conf
 serverConf = "server.conf"
+serverStartConf = "serverStart.conf"
 
 # 返回部署工程的目标目录
 def deploymentTomcatName(serverName):
@@ -212,28 +214,40 @@ def writeLog(log_file,loginfo):
             fd.write(loginfo)
 
 # 读取配置文件设置需要部署的服务
-def readConf():
-    import ConfigParser
+def readConf(confPath):
     cf = ConfigParser.ConfigParser()
-    cf.read(serverConf)
+    cf.read(confPath)
     serverNameDict = {}
+    serverNameList = []
     portDict = {}
     for serverName in cf.sections():
         #print 'serverName:%s' % serverName
         for optins in cf.options(serverName):
             # 取服务名下的对应的配置和参数
             port = cf.get(serverName, optins)
-            #print optins, port
             portDict[optins] = port
         serverNameDict[serverName] = portDict
-        portDict= {}
-    return serverNameDict
+        serverNameList.append(serverNameDict)
+        #print serverNameDict
+        portDict={}
+        serverNameDict ={}
+    return serverNameList
+
+# 读取启动服务顺序配置文件
+def readStartServerConf():
+    cf = ConfigParser.ConfigParser()
+    cf.read(serverStartConf)
+    serverList = []
+    for serverName in cf.sections():
+        serverList.append(serverName)
+
+    return serverList
 
 #部署主函数
 def deploy(Tag):
-    serverConfPath = os.path.exists(os.path.join(os.getcwd(),serverConf))
-    #print serverConf
-    if not serverConfPath:
+    serverConfPath = os.path.join(os.getcwd(),serverConf)
+    #print serverConfPath
+    if not os.path.exists(serverConfPath):
         print "serverconf is not exists,check serverconf %s "% serverConfPath
         print """ %s like this:
                    [servername]
@@ -242,118 +256,65 @@ def deploy(Tag):
                    shutdown_port = 8830""" % serverConf
         sys.exit()
     # 读取配置文件需要部署的服务名，根据设置的端口部署服务
-    serverNameDict = readConf()
-
-    # s = {
-    #      'b2b-trade-api':{'http_port': '8048', 'shutdown_port': '8248', 'ajp_port': '8148'},
-    #      'goods-gtin-goods-api':{'http_port': '8061', 'shutdown_port': '8805', 'ajp_port': '8008'},
-    #      'b2b-activity-oss':{'http_port': '8044', 'shutdown_port': '8244', 'ajp_port': '8144'},
-    #      'tms-api':{'http_port': '8031', 'shutdown_port': '8513', 'ajp_port': '8413'},
-    #      'goods-gtin-api':{'http_port': '8059', 'shutdown_port': '8705', 'ajp_port': '8007'},
-    #      'b2b-shop':{'http_port': '8040', 'shutdown_port': '8240', 'ajp_port': '8140'},
-    #      'pos-h5-oss':{'http_port': '8086', 'shutdown_port': '8005', 'ajp_port': '8019'},
-    #      'upload':{'http_port': '8080', 'shutdown_port': '8345', 'ajp_port': '8009'},
-    #      'b2b-shop-oss':{'http_port': '8042', 'shutdown_port': '8242', 'ajp_port': '8142'},
-    #      'finance-api':{'http_port': '8056', 'shutdown_port': '8305', 'ajp_port': '8003'},
-    #      'finance-oss':{'http_port': '8065', 'shutdown_port': '8274', 'ajp_port': '8174'},
-    #      'open':{'http_port': '8888', 'shutdown_port': '8011', 'ajp_port': '8111'},
-    #      'member-api':{'http_port': '8092', 'shutdown_port': '8125', 'ajp_port': '8211'},
-    #      'dts-api':{'http_port': '8010', 'shutdown_port': '8053', 'ajp_port': '8033'},
-    #      'b2b-goods-api':{'http_port': '8047', 'shutdown_port': '8247', 'ajp_port': '8147'},
-    #      'goods-channel-api':{'http_port': '8066', 'shutdown_port': '8505', 'ajp_port': '8335'},
-    #      'goods-channel-oss':{'http_port': '8054', 'shutdown_port': '8605', 'ajp_port': '8006'},
-    #      'b2b-shop-h5-oss':{'http_port': '8045', 'shutdown_port': '8245', 'ajp_port': '8445'},
-    #      'main-oss':{'http_port': '8081', 'shutdown_port': '8213', 'ajp_port': '8113'},
-    #      'help-api':{'http_port': '8072', 'shutdown_port': '8272', 'ajp_port': '8172'},
-    #      'member-h5':{'http_port': '8073', 'shutdown_port': '8273', 'ajp_port': '8273'},
-    #      'tms-oss':{'http_port': '8043', 'shutdown_port': '8143', 'ajp_port': '8243'},
-    #      'shopcart-api':{'http_port': '8032', 'shutdown_port': '8532', 'ajp_port': '8532'},
-    #      'goods-shop-api':{'http_port': '8088', 'shutdown_port': '8115', 'ajp_port': '8110'},
-    #      'store-oss':{'http_port': '8094', 'shutdown_port': '8185', 'ajp_port': '8017'},
-    #      'xcode-oss':{'http_port': '8064', 'shutdown_port': '8555', 'ajp_port': '8015'},
-    #      'xcode-h5':{'http_port': '7070', 'shutdown_port': '8225', 'ajp_port': '8020'},
-    #      'common-oss':{'http_port': '8096', 'shutdown_port': '8655', 'ajp_port': '8355'},
-    #      'job':{'http_port': '8585', 'shutdown_port': '8014', 'ajp_port': '8214'},
-    #      'shop-h5-oss':{'http_port': '8051', 'shutdown_port': '8105', 'ajp_port': '8217'},
-    #      'bmhc-api':{'http_port': '8099', 'shutdown_port': '8333', 'ajp_port': '8021'},
-    #      'sso':{'http_port': '9999', 'shutdown_port': '8395', 'ajp_port': '8210'},
-    #      'pos-oss':{'http_port': '8085', 'shutdown_port': '8035', 'ajp_port': '8049'},
-    #      'help-h5':{'http_port': '8067', 'shutdown_port': '8252', 'ajp_port': '8152'},
-    #      'pos-bill-api':{'http_port': '8057', 'shutdown_port': '8145', 'ajp_port': '8013'},
-    #      'xcode-api':{'http_port': '8098', 'shutdown_port': '8215', 'ajp_port': '8219'},
-    #      'job-console':{'http_port': '8356', 'shutdown_port': '8256', 'ajp_port': '8156'},
-    #      'xcode-h5-oss':{'http_port': '8052', 'shutdown_port': '8235', 'ajp_port': '8018'},
-    #      'tms-h5-oss':{'http_port': '8046', 'shutdown_port': '8246', 'ajp_port': '8146'},
-    #      'gbsi-api':{'http_port': '8060', 'shutdown_port': '8906', 'ajp_port': '8315'},
-    #      'finance-flows-api':{'http_port': '8058', 'shutdown_port': '8405', 'ajp_port': '8004'},
-    #      'gtin-oss':{'http_port': '8063', 'shutdown_port': '8905', 'ajp_port': '8109'},
-    #      'activity-api':{'http_port': '8095', 'shutdown_port': '8205', 'ajp_port': '8001'},
-    #      'store-api':{'http_port': '8074', 'shutdown_port': '8175', 'ajp_port': '8016'},
-    #      'sms-oss':{'http_port': '8041', 'shutdown_port': '8241', 'ajp_port': '8141'},
-    #      'help-oss':{'http_port': '8062', 'shutdown_port': '8262', 'ajp_port': '8162'},
-    #      'trade-api':{'http_port': '8090', 'shutdown_port': '8195', 'ajp_port': '8118'},
-    #      'shop-h5':{'http_port': '8083', 'shutdown_port': '8155', 'ajp_port': '8314'},
-    #      'activity-oss':{'http_port': '8055', 'shutdown_port': '8325', 'ajp_port': '8002'},
-    #      'sms-api':{'http_port': '8030', 'shutdown_port': '8230', 'ajp_port': '8130'},
-    #      'b2b-activity-api':{'http_port': '8039', 'shutdown_port': '8239', 'ajp_port': '8139'},
-    #      'pos-api':{'http_port': '8093', 'shutdown_port': '8135', 'ajp_port': '8012'},
-    #      'shop-oss':{'http_port': '8084', 'shutdown_port': '8165', 'ajp_port': '8515'},}
+    serverNameList = readConf(serverConfPath)
+    #print serverNameList
     info = ""
-    for serverName, portDict in serverNameDict.iteritems():
-        if serverName == "conf":
-            # 如果是conf 的就略过，下一个服务，conf 是做为配置文件的配置
-            continue
-        shutdown_port = portDict["shutdown_port"]
-        http_port = portDict["http_port"]
-        ajp_port = portDict["ajp_port"]
-        if Tag == "reinstall":
-            #清理老的部署文件，重新部署
-            if checkServer(serverName):
-                 stopServerName(serverName)
-                 time.sleep(1)
-                 cleanFile(serverName)
-                 # 从标准tomcat 复制到部署目录
-                 copyBaseTomcat(serverName)
-                 # 修改部署tomcat server.xml配置文件
-                 changeXml(serverName, shutdown_port=shutdown_port, http_port=http_port, ajp_port=ajp_port)
-                 # 检查服务是否注册，
-            if not checkServer(serverName):
-                installServer(serverName, 'uninstall')
-                installServer(serverName, 'install')
-            else:
-                print "%s is installed" %serverName
-
-        elif Tag =="install":
-            # 检查服务是否注册，
-            if not checkServer(serverName):
-                # 从标准tomcat 复制到部署目录
-                copyBaseTomcat(serverName)
-                # 修改部署tomcat server.xml配置文件
-                changeXml(serverName, shutdown_port=shutdown_port, http_port=http_port, ajp_port=ajp_port)
-                installServer(serverName, 'install')
+    for serverNameDict in serverNameList:
+        # print serverNameDict
+        for serverName, portDict in serverNameDict.iteritems():
+            if serverName == "conf":
+                # 如果是conf 的就略过，下一个服务，conf 是做为配置文件的配置
+                continue
+            shutdown_port = portDict["shutdown_port"]
+            http_port = portDict["http_port"]
+            ajp_port = portDict["ajp_port"]
+            if Tag == "reinstall":
+                #清理老的部署文件，重新部署
                 if checkServer(serverName):
-                    print "server:%s install Sucess" % serverName
-                else:
-                    print "server:%s install Fail" % serverName
-            else:
-                print "%s is installed" % serverName
-        elif Tag == "uninstall":
-            if checkServer(serverName):
-                stopServerName(serverName)
-                installServer(serverName, 'uninstall')
-                cleanFile(serverName) # 清理老的部署文件，注销服务
+                     stopServerName(serverName)
+                     time.sleep(1)
+                     cleanFile(serverName)
+                     # 从标准tomcat 复制到部署目录
+                     copyBaseTomcat(serverName)
+                     # 修改部署tomcat server.xml配置文件
+                     changeXml(serverName, shutdown_port=shutdown_port, http_port=http_port, ajp_port=ajp_port)
+                     # 检查服务是否注册，
                 if not checkServer(serverName):
-                    print "server:%s uninstall Sucess" % serverName
+                    installServer(serverName, 'uninstall')
+                    installServer(serverName, 'install')
                 else:
-                    print "server:%s uninstall fail" % serverName
+                    print "%s is installed" %serverName
+            elif Tag =="install":
+                # 检查服务是否注册，
+                if not checkServer(serverName):
+                    # 从标准tomcat 复制到部署目录
+                    copyBaseTomcat(serverName)
+                    # 修改部署tomcat server.xml配置文件
+                    changeXml(serverName, shutdown_port=shutdown_port, http_port=http_port, ajp_port=ajp_port)
+                    installServer(serverName, 'install')
+                    if checkServer(serverName):
+                        print "server:%s install Sucess" % serverName
+                    else:
+                        print "server:%s install Fail" % serverName
+                else:
+                    print "%s is installed" % serverName
+            elif Tag == "uninstall":
+                if checkServer(serverName):
+                    stopServerName(serverName)
+                    installServer(serverName, 'uninstall')
+                    cleanFile(serverName) # 清理老的部署文件，注销服务
+                    if not checkServer(serverName):
+                        print "server:%s uninstall Sucess" % serverName
+                    else:
+                        print "server:%s uninstall fail" % serverName
+                else:
+                    print "server:%s not install!" % serverName
             else:
-                print "server:%s not install!" % serverName
-        else:
-            pass
+                pass
 
 # 初始化 读取配置文件配置
 def _init():
-    serverConf = readConf()
+    serverConfPath = readConf(serverConf)
     deploymentDir = serverConf["conf"]["deploymentdir"]
     baseDeploymentName = serverConf["conf"]["basedeploymentname"]
     baseTomcat = serverConf["conf"]["basetomcat"]
@@ -369,19 +330,24 @@ def list_dir(path):
             ser_dict[service_name] = service_name_path
     return ser_dict
 
+
 if __name__ == "__main__":
     # 读取配置文件信息
-    deploymentDir, baseDeploymentName, baseTomcat = _init()
-    try:
-        tag = sys.argv[1]
-    except:
-        print "follow a agrs,install,uninstall,reinstall"
-        sys.exit()
-    #  sername  tag
-    if tag in ["install", "uninstall", "reinstall"]:
-        deploy(tag)
-    else:
-        print " only install,uninstall,reinstall"
+    #print readStartServerConf()
+    #print readConf(serverConf)
+    tag = sys.argv[1]
+    deploy(tag)
+    # deploymentDir, baseDeploymentName, baseTomcat = _init()
+    # try:
+    #     tag = sys.argv[1]
+    # except:
+    #     print "follow a agrs,install,uninstall,reinstall"
+    #     sys.exit()
+    # #  sername  tag
+    # if tag in ["install", "uninstall", "reinstall"]:
+    #     deploy(tag)
+    # else:
+    #     print " only install,uninstall,reinstall"
 
 
 
