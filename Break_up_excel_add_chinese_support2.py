a0284af994746fed1ced4bae75b8fd992050b7a1
silenceGuo <516605659@qq.com>
2019-03-07 14:15:04 +0800

    ｕｐｄａｔｅ

--- Break_up_excel_add_chinese_support2.py
+++ Break_up_excel_add_chinese_support2.py
@@ -3,10 +3,10 @@
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
-sys.path.append("D:\Python27\Lib\site-packages")
+# sys.path.append("D:\Python27\Lib\site-packages")
 import pandas as pd
-import xlrd
-import openpyxl
+# import xlrd
+# import openpyxl
 import os
 
 # 以指定列名的值拆分Excel 以拆分列名的值输出Excel
@@ -16,9 +16,7 @@ def readExcel(File,sheetName):
         print "%s is not exitis" % File
         sys.exit(1)
     read_temple = pd.ExcelFile(File, dtype=str)
-
     read_temple_sheet = read_temple.parse(sheetName.decode("utf-8"),dtype=str)
-
     return read_temple_sheet
 
 
@@ -27,6 +25,7 @@ def readExcelSUB(File):
     if not os.path.exists(File):
         print "%s is not exitis" % File
         sys.exit(1)
+    print "读取总账单数据 %s" % File
     read_temple = pd.ExcelFile(File, dtype=str)
     # read_temple = pd.ExcelFile(File, dtype=str, low_memory=False)
     sheets = read_temple.sheet_names
@@ -39,7 +38,7 @@ def readExcelSUB(File):
         #     read_temple_sheet['订单号'.decode("utf-8")] = read_temple_sheet['订单号'.decode("utf-8")].astype('str')
         sheetobjectDict[sheet] = read_temple_sheet
         # print read_temple_sheet
-
+    print "读取完成，下一步拆分"
     return sheets, sheetobjectDict
 
 # 拆分execl 已公司名称拆分
@@ -47,7 +46,7 @@ def writeExcel(sheetobjectDict,dstpath,sheetName,company_name):
     # 过滤条件
     filter_li = '公司名称'.decode("utf-8")
 
-    path = os.path.join(dstpath, "%s_%s_%s.xlsx".decode("utf-8")) % (souerFilename, company_name, "账单")
+    path = os.path.join(dstpath, "%s_%s.xlsx".decode("utf-8")) % (souerFilename, company_name)
 
     writer = pd.ExcelWriter(path)
 
@@ -58,6 +57,7 @@ def writeExcel(sheetobjectDict,dstpath,sheetName,company_name):
             read_temple_sheet_filter = read_temple_sheet[read_temple_sheet[filter_li] == company_name]
         except KeyError, e:
             # print type(e)
+            print e
             print "过滤列名:%s， 错误" % filter_li
             # print " 列名错误 err"
             sys.exit(1)
@@ -80,12 +80,14 @@ def main(file,dstpath):
 
     sheetlist,sheetobjectDict = readExcelSUB(file)
 
-
     CompanyList = getCompanyName(sheetobjectDict)
-    print "公司数量：%s" % len(CompanyList)
+    # print "公司数量：%s" % len(CompanyList)
     for Company in CompanyList:
-        # print Company
+        print "开始拆分：%s 账单" % Company
+        # print "拆分进度 %s"
         writeExcel(sheetobjectDict,dstpath,sheetlist, Company)
+    print "拆分完成,输出目录：%s " % dstpath
+    print "拆分公司账单数量：%s" % len(CompanyList)
 
 if __name__ == "__main__":
     # 源文件 主要 要保证总账单清理多余的行 公司名称列与其他表格名称一直，
